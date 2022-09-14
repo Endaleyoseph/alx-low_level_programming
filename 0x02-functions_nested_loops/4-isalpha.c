6df5df778035a7e1a7a3b6b0d1878c8c59115fae
Endaleyoseph <endaleyoseph1@gmail.com>
2022-09-14 07:58:16 -0700

    pain

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,7 +9,7 @@
 int _isalpha(int c)
 {
 
-	if ((c >= 65 && c <= 9)0 || (c >= 97 && c <= 122))
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 	{
 
 		return (1);
@@ -23,4 +23,4 @@ int _isalpha(int c)
 
 	}
 
-}
+}i
