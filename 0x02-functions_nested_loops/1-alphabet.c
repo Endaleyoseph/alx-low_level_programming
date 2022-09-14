1243e4fa242c77256f41a1626f635e6842fc7884
Endaleyoseph <endaleyoseph1@gmail.com>
2022-09-14 06:01:08 -0700

    alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+
+		_putchar(ch);
+
+	}
+
+	_putchar('\n');
+
+}
