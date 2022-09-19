29d87dac8b318eb2333869394ab6ca276c0ff5ae
Endaleyoseph <endaleyoseph1@gmail.com>
2022-09-19 14:33:49 -0700

    fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,11 +10,11 @@ void print_rev(char *s)
 {
 	int a, b = 0;
 
-	for(a = 0; *(s + a) != '\0'; a++)
+	for (a = 0; *(s + a) != '\0'; a++)
 	{
 		b++;
 	}
-	for(a = b - 1; a >= 0; a--)
+	for (a = b - 1; a >= 0; a--)
 	{
 		_putchar(*(s + a));
 	}
