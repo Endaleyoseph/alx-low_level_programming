99f8abc6d2e01b4f2770f84c20796b642a81fea6
Endaleyoseph <endaleyoseph1@gmail.com>
2022-09-27 05:48:17 -0700

    fix

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#define NULL 0
 
 /**
  * _strchr -  locates a character in a string
@@ -12,12 +11,16 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; *(s + i); i++)
 	{
-		if (s[i] == c)
+		if (*(s + i) == c)
 		{
-			return (s + i);
+			break;
 		}
 	}
-	return (NULL);
+	if (*(s + i) == c)
+	{
+		return (s + i);
+	}
+	return ('\0');
 }
