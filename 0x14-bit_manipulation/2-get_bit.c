659bddca17bdf6e0a1e9c2bec1d9a5569c473f1e
Yoseph Endale <endaleyoseph1@gmail.com>
2022-10-27 06:32:43 -0700

    get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -5,12 +5,19 @@
  * @n: number in decimal
  * @index: index at which bit is taken
  * Return: 1 if it exists, 0 if not
+ * -1 when error occures
  */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (n & (1 << index))
-		return (1);
+	unsigned int max_bit;
+	int bit;
 
-	return (0);
+	max_bit = (sizeof(unsigned long int) * 8);
+	if (index > max_bit)
+		return (-1);
+
+	bit = ((n >> index) & 1);
+
+	return (bit);
 }
