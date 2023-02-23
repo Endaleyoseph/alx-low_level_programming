88335ed820e610ff559a9d048e7575271de1e5f7
Yoseph Endale <endaleyoseph1@gmail.com>
2023-02-23 03:34:00 -0800

    fix 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -13,12 +13,12 @@ def island_perimeter(grid):
     for i in range(row):
         for j in range(col):
             if grid[i][j] == 1:
-                if j > 0 and grid[i][j - 1] == 0:
+                if j == 0 or grid[i][j - 1] == 0:
                     per += 1
-                if j < col - 1 and grid[i][j + 1] == 0:
+                if j == col - 1 or grid[i][j + 1] == 0:
                     per += 1
-                if i > 0 and grid[i - 1][j] == 0:
+                if i == 0 or grid[i - 1][j] == 0:
                     per += 1
-                if i < row - 1 and grid[i - 1][j] == 0:
+                if i == row - 1 or grid[i - 1][j] == 0:
                     per += 1
     return per
